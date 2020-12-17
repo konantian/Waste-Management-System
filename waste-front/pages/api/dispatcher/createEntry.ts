e0700b87836b71e301b2605e0c2d983ee7631aa3
Yuan <wang17@ualberta.ca>
2020-12-17 16:21:41 +0800

    Finish createEntry api

--- waste-front/pages/api/dispatcher/createEntry.ts
+++ waste-front/pages/api/dispatcher/createEntry.ts
@@ -1,6 +1,6 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import prisma from '../../../lib/prisma';
-import { create_entry } from '../../../utils/dispatcherUtil';
+import { create_entry, check_own_truck, check_truck} from '../../../utils/dispatcherUtil';
 
 export default async function createEntry(req : NextApiRequest, res : NextApiResponse ){
 
@@ -8,7 +8,20 @@ export default async function createEntry(req : NextApiRequest, res : NextApiRes
         return res.status(405).json({error : "Method not allowed, please use POST"});
     }
 
+    const owned_truck_id = await check_own_truck(prisma, req.body.driver_id);
+    const validTruck = await check_truck(prisma, req.body.truck_id);
+    if(!owned_truck_id && !validTruck){
+        await prisma.$disconnect();
+        return res.status(400).json({error : "The truck entered is not available,please enter again"});
+    }
+
+    const addEntry = await create_entry(prisma, req.body);
+    if(!addEntry){
+        await prisma.$disconnect();
+        return res.status(400).json({error : "This entry cannot be created, please check you data"});
+    }
     
+    await prisma.$disconnect();
     return res.status(201).json({success : "New entry has been created"});
 
 }
--- waste-front/utils/dispatcherUtil.js
+++ waste-front/utils/dispatcherUtil.js
@@ -20,6 +20,35 @@ export const get_pick_up = async (prisma, master_account) => {
     return result.cid_drop_off;
 }
 
+export const check_own_truck = async (prisma, driver_id) => {
+
+    const result = await prisma.driver.findFirst({
+        where : {pid : driver_id},
+        select : {owned_truck_id : true}
+    });
+
+    return result.owned_truck_id;
+}
+
+export const check_truck = async (prisma, truck_id) => {
+
+    const owned = await prisma.driver.findMany({
+        where : {owned_truck_id : {not : 'NULL'} },
+        select : {owned_truck_id : true}
+    })
+
+    const owned_trucks = owned.map(item => item.owned_truck_id);
+
+    const result = await prisma.truck.findFirst({
+        where : {truck_id : {notIn : owned_trucks},
+                 truck_id : truck_id},
+    });
+
+    if(result) return true;
+    return false;
+    
+}
+
 export const get_waste_types = async (prisma, master_account) => {
 
     const result = await prisma.serviceAgreement.findMany({
@@ -37,50 +66,26 @@ export const get_available_containers = async (prisma, agreement) => {
 
     const waste_types = await get_waste_types(prisma, master_account);
 
-    const drop_off_result = await prisma.serviceFulfillment.findMany({
-        select : {cid_drop_off : true}
-    });
+    const result = await prisma.$queryRaw('SELECT c.container_id FROM "Container" c WHERE NOT EXISTS (SELECT * FROM "ServiceFulfillment" s WHERE s.cid_drop_off = c.container_id) \
+     UNION SELECT c.container_id FROM "Container" c WHERE (SELECT MAX(date_time) FROM "ServiceFulfillment" s WHERE s.cid_pick_up = c.container_id) > \
+    (SELECT MAX(date_time) FROM "ServiceFulfillment" s WHERE s.cid_drop_off = c.container_id)')
 
-    const drop_off_containers = drop_off_result.map(item => item.cid_drop_off);
+    const unavailable_containers = result.map(item => item.container_id);
 
-    const available_result = await prisma.container.findMany({
-        where : { 
-            container_id : {
-                notIn : drop_off_containers,
-            }
+    const available = await prisma.containerWasteType.findMany({
+        where : {
+            container : {container_id : {in : unavailable_containers}},
+            waste_type : {in : waste_types}
         },
         select : {container_id : true}
     });
 
-    const available = available_result.map(item => item.container_id);
+    const available_containers = available.map(item => item.container_id);
 
-    let waste_result = await Promise.all(waste_types.map(async (type) => {
-        const result = await prisma.containerWasteType.findMany({
-            where : {waste_type : type},
-            select : {container_id : true}
-        });
-        return result.map(item => item.container_id);
-    }))
-
-    let waste_containers = [];
-    waste_result.forEach(item => {
-        item.forEach(cid => {
-            waste_containers.push(cid);
-        })
-    })
-
-    const available_containers = waste_containers.map(cid => {
-        if(available.includes(cid)) return cid;
-    })
-
-    const containers = [];
-    available_containers.forEach(item => {
-        if(item) containers.push(item);
-    })
-    return [...new Set(containers)];
+    return available_containers;
 }
 
-export const create_entry = async (prisma,data) => {
+export const create_entry = async (prisma, data) => {
 
     const {date_time, agreement, truck_id, driver_id, cid_drop_off} = data;
 
@@ -89,7 +94,7 @@ export const create_entry = async (prisma,data) => {
 
     const addEntry = await prisma.serviceFulfillment.create({
         data : {
-            data_time : date_time,
+            date_time : date_time,
             master_account : master_account,
             service_no : agreement,
             cid_drop_off : cid_drop_off,
